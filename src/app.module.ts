4e5561b9faee5b6d81fe541a57f0f58de6c71559
josue <josue.ramirez.z@uni.pe>
2022-06-06 17:41:54 -0500

    configuracion de react, webpack, babel para el frontend

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReposApiController } from './controllers/repos-api/repos-api.controller';
 import { ProductService } from './services/githubApi.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'public')
+  })],
   controllers: [AppController, ReposApiController],
   providers: [AppService, ProductService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv'
+import { createTokenAuth } from '@octokit/auth-token'
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path'
 config()
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'))
+  app.setViewEngine('jsx');
+  app.enableCors()
   await app.listen(3001);
+
 }
 bootstrap();
+
+// async function getToken() {
+//   const auth = createTokenAuth(`${process.env.GUTHUB_TOKEN}`);
+//   return await auth();
+
+// };
+// const token = getToken()
+// export { token }
